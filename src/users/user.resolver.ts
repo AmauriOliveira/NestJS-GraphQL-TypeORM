614a88f27b97a2c413bbe133720c219cc4b7cd5d
Amauri Oliveira <amaurioliveira@tokenlab.com.br>
2021-01-28 21:06:57 -0300

    Adiciona Query Pesquisar um usuário por ID e todos usuários. Adicona Mutation Adicionar usuário novo.

---
+++ src/users/user.resolver.ts
@@ -0,0 +1,28 @@
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { CreateUserInput } from './dtos/create-user.input';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Resolver('User')
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    const users = await this.userService.findAll();
+    return users;
+  }
+
+  @Query(() => User)
+  async user(@Args('id') id: string): Promise<User> {
+    const user = await this.userService.findUserById(id);
+    return user;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+    const user = await this.userService.createUser(data);
+
+    return user;
+  }
+}
---
+++ src/users/user.service.ts
@@ -0,0 +1,44 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dtos/create-user.input';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    const users = this.userRepository.find();
+
+    return users;
+  }
+
+  async findUserById(id: string): Promise<User> {
+    const user = await this.userRepository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado.');
+    }
+
+    return user;
+  }
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    const user = await this.userRepository.create(data);
+    const userSaved = await this.userRepository.save(user);
+
+    if (!userSaved) {
+      throw new InternalServerErrorException('Problema para criar um usuário.');
+    }
+
+    return userSaved;
+  }
+}
